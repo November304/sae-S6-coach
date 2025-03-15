4e020606c96906d6c3f1fe6c7092d6c03045b513
thomasduppi <126145508+thomasduppi@users.noreply.github.com>
2025-03-15 21:05:08 +0100

    design planning avec filtre fonctionnels

--- backend/migrations/Version20250314140324.php
+++ backend/migrations/Version20250314140324.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250314083921 extends AbstractMigration
+final class Version20250314140324 extends AbstractMigration
 {
     public function getDescription(): string
     {
